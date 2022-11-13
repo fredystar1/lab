572f4ca2fe31fabf28b0e9a504bab4ad51dc4986
fredystar1 <118084826+fredystar1@users.noreply.github.com>
2022-11-12 18:58:27 -0600

    added second file

--- account.py
+++ account.py
@@ -1,12 +1,22 @@
 class Account:
-    def __init__ (self,name):
+    def __init__ (self,name: str) -> None:
+        """
+        this is a function to set the default values
+        :param name: holds the account name
+        """
+
         self.__account_name = name
         self.__account_balance = 0
 
 
 
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        this function allows you to deposit money into account balance
+        :param amount: amount you want to deposit
+        :return: if account deposit was worked or failed
+        """
         self.amount = amount
         if self.amount > 0:
             self.__account_balance = self.__account_balance + amount
@@ -16,7 +26,12 @@ class Account:
 
 
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        function that allows you to withdraw from account balance
+        :param amount: amount you want to withdraw
+        :return: if withdrawl worked or failed
+        """
         if (amount < 0) or (amount > self.__account_balance):
             return False
         else:
@@ -24,9 +39,17 @@ class Account:
             return True
 
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        function that allows you to check your account balance
+        :return: returns account balance
+        """
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        function that allows you to check the account name
+        :return: the account name
+        """
         return self.__account_name
 
